5ae81ce9ada8e96788bdd5e50213514c1e267641
Alejo Zuleta <jhoitan266@gmail.com>
2024-04-14 01:09:24 -0500

    feature,style: responsive and styles

--- src/app/components/specific-components/navegation/navbar/navbar.component.ts
+++ src/app/components/specific-components/navegation/navbar/navbar.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartShopComponent } from '../cart-shop/cart-shop.component';
 import { HamburgerMenuComponent } from '../hamburger-menu/hamburger-menu.component';
+import { RouterLink } from '@angular/router';
+
 
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,HamburgerMenuComponent,CartShopComponent],
+  imports: [CommonModule,RouterLink,HamburgerMenuComponent,CartShopComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -19,17 +21,17 @@ export class NavbarComponent {
     {
       title: 'Inicio',
       img:'assets/icon/home.svg',
-      url: 'home'
+      url: '/home'
     },
     {
       title: 'productos',
       img:'assets/icon/product.svg',
-      url: 'products'
+      url: '/products'
     },
     {
       title: 'Favoritos',
       img:'assets/icon/heat.svg',
-      url: 'favorite'
+      url: '/favorite'
     },
   ]; 
 }
